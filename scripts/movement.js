7b5674a0419a92f09d518b6f17847ed150a24e9a
Bryzz <alarangaerlan@gmail.com>
2016-09-19 19:48:14 +0200

    Switched keyboard to mouse+clic controls. Added new pictures.

--- scripts/movement.js
+++ scripts/movement.js
@@ -3,42 +3,22 @@
 var displacement = 368; // width is 720, player's ship width is 32. Value = (720 / 2) - (32 / 2)
 var hasShot = false;
 var step = 0;
+var cvHasBeenPlaced = false;
 
 $("#playerShip").css('margin-left', displacement); // placing ship
 $("#playerProjectile").css("display", "none"); // hiding projectile
 
-document.onkeydown = action;
-
-function action(e) {
-
-  if (e.keyCode == '37') {
-    goLeft();
-  }
-  else if (e.keyCode == '39') {
-    goRight();
-  }
-  else if (e.keyCode == '32') {
-    shoot();
-  }
+document.getElementById("areaForPlayerShip").onmousemove = function(e) {
+    document.getElementById("playerShip").style.marginLeft = e.pageX*1 - 590 + "px";
+    if (document.getElementById("playerShip").style.marginLeft.replace("px","") < 0) {
+      document.getElementById("playerShip").style.marginLeft = 0;
+    }
+    if (document.getElementById("playerShip").style.marginLeft.replace("px","") > 748) {
+      document.getElementById("playerShip").style.marginLeft = 748+"px";
+    }
 }
 
-function goLeft() {
-  displacement -= 10;
-  if (displacement < 0) {
-    displacement = 0;
-  }
-  $("#playerShip").css('margin-left', displacement);
-}
-
-function goRight() {
-  displacement += 10;
-  if (displacement > 748) {
-    displacement = 748;
-  }
-  $("#playerShip").css('margin-left', displacement);
-}
-
-function shoot() {
+document.getElementById("areaForPlayerShip").onclick = function(e) {
   if (hasShot == false) {
     $("#playerProjectile").css("display", "");
     $("#playerProjectile").css("top", $("#playerShip").offset().top - 12);
@@ -61,19 +41,50 @@ function checkCollision() {
       $("#playerProjectile").css("top", "0");
       hasShot = false;
     }
-    else if ( !((($("#playerProjectile").offset().top + $("#playerProjectile").height()) < ($("#enemyShip").offset().top)) ||
-        ($("#playerProjectile").offset().top > ($("#enemyShip").offset().top + $("#enemyShip").height())) ||
-        (($("#playerProjectile").offset().left + $("#playerProjectile").width()) < $("#enemyShip").offset().left) ||
-        ($("#playerProjectile").offset().left > ($("#enemyShip").offset().left + $("#enemyShip").width())))) {
-      // the projectile hits a CV. Good job, you just ruined someone's hopes.
+    else if ( !((($("#playerProjectile").offset().top + $("#playerProjectile").height()) < ($("#enemyShipSkills").offset().top)) ||
+        ($("#playerProjectile").offset().top > ($("#enemyShipSkills").offset().top + $("#enemyShipSkills").height())) ||
+        (($("#playerProjectile").offset().left + $("#playerProjectile").width()) < $("#enemyShipSkills").offset().left) ||
+        ($("#playerProjectile").offset().left > ($("#enemyShipSkills").offset().left + $("#enemyShipSkills").width())))) {
+      // the projectile hits the skills enemy. Good job, you just ruined someone's talents.
       $("#playerProjectile").css("display", "none");
       $("#playerProjectile").css("top", "0");
-      $("#explosion").css("top", $("#enemyShip").offset().top - 26);
-      $("#explosion").css("left", $("#enemyShip").offset().left - 19);
+      $("#explosion").css("top", $("#enemyShipSkills").offset().top - 25);
+      $("#explosion").css("left", $("#enemyShipSkills").offset().left - 10);
       $("#explosion").attr("src","");
-      $("#enemyShip").css("display","none");
+      $("#enemyShipSkills").css("display","none");
       $("#explosion").attr("src","img/ship/cv_enemy_explosion.gif");
-      // TODO
+      hasShot = false;
+      showSkills();
+    }
+    else if ( !((($("#playerProjectile").offset().top + $("#playerProjectile").height()) < ($("#enemyShipExperience").offset().top)) ||
+        ($("#playerProjectile").offset().top > ($("#enemyShipExperience").offset().top + $("#enemyShipExperience").height())) ||
+        (($("#playerProjectile").offset().left + $("#playerProjectile").width()) < $("#enemyShipExperience").offset().left) ||
+        ($("#playerProjectile").offset().left > ($("#enemyShipExperience").offset().left + $("#enemyShipExperience").width())))) {
+      // the projectile hits the experience's enemy. Good job, you just ruined someone's history.
+      $("#playerProjectile").css("display", "none");
+      $("#playerProjectile").css("top", "0");
+      $("#explosion").css("top", $("#enemyShipExperience").offset().top - 25);
+      $("#explosion").css("left", $("#enemyShipExperience").offset().left - 10);
+      $("#explosion").attr("src","");
+      $("#enemyShipExperience").css("display","none");
+      $("#explosion").attr("src","img/ship/cv_enemy_explosion.gif");
+      hasShot = false;
+      showExperience();
+    }
+    else if ( !((($("#playerProjectile").offset().top + $("#playerProjectile").height()) < ($("#enemyShipFormation").offset().top)) ||
+        ($("#playerProjectile").offset().top > ($("#enemyShipFormation").offset().top + $("#enemyShipFormation").height())) ||
+        (($("#playerProjectile").offset().left + $("#playerProjectile").width()) < $("#enemyShipFormation").offset().left) ||
+        ($("#playerProjectile").offset().left > ($("#enemyShipFormation").offset().left + $("#enemyShipFormation").width())))) {
+      // the projectile hits the formation's enemy. Good job, you just ruined someone's studies.
+      $("#playerProjectile").css("display", "none");
+      $("#playerProjectile").css("top", "0");
+      $("#explosion").css("top", $("#enemyShipFormation").offset().top - 25);
+      $("#explosion").css("left", $("#enemyShipFormation").offset().left - 10);
+      $("#explosion").attr("src","");
+      $("#enemyShipFormation").css("display","none");
+      $("#explosion").attr("src","img/ship/cv_enemy_explosion.gif");
+      hasShot = false;
+      showFormation();
     }
   }
 }
